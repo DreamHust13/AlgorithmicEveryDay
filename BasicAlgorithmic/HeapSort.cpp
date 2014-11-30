62356ee11759988357c27336f595205ae2ac9bef
DreamHust13 <1506732332@qq.com>
2014-11-30 21:46:39 +0800

    堆排序
    改错

--- BasicAlgorithmic/HeapSort.cpp
+++ BasicAlgorithmic/HeapSort.cpp
@@ -1,7 +1,12 @@
 /*堆排序
- *
+ *时间复杂度：O(NlogN)
  *堆是完全二叉树；且有大小堆之分
  *注意：数组从1开始，因为这样才满足:对于节点i，其左孩子是2*i,右孩子是2*i+1
+ *应用：最大堆排序用来从小到大排序数列
+ *扩展：堆经常被用来求一个数列中的第K大的数，只需要建立一个大小为K的最小堆，堆顶就是第K大的数；
+ *		(如：选前k个数建最小堆，若后面的数比堆顶小，舍弃；若比堆顶大，舍弃堆顶，将数据插入，调整最小堆)！！！想清楚
+ *					求数列中第K小的数，建立大小为K的最大堆，堆顶就是第K小的数。复杂度为O(NlogK)
+ *					//参见《编程之美》Chapter5的第五节
 */
 
 #include <iostream>
@@ -18,11 +23,11 @@ void MaxHeapify(int *heapdata,int i,int size)//调整堆
 
     if(i <= size/2 && i>=1)//如果i为叶节点，则不需要调整
     {
-        if( lchild<=size && heapdata[lchild]>max )
+        if( lchild<=size && heapdata[lchild]>heapdata[max] )//!!!是数据比，而不是与max比!!!
         {
             max = lchild;
         }
-        if( rchild<=size && heapdata[rchild]>max)
+        if( rchild<=size && heapdata[rchild]>heapdata[max])
         {
             max = rchild;
         }
@@ -48,7 +53,7 @@ void HeapSort(int *heapdata,int size)//堆排序
 
     BuildMaxHeap(heapdata,size);//建堆
 
-    for(i=size;i<=2;i--)
+    for(i=size;i>=2;i--)//!!!又是大于号的问题！！！
     {
         swap(heapdata[i],heapdata[1]);//交换堆顶和最后一个元素，即每次将剩余元素中的最大者放在最后
         MaxHeapify(heapdata,1,i-1);//重新调整堆顶节点，生成最大堆
