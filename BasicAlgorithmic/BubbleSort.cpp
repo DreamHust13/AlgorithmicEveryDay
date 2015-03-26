979f17e00f3d4ade35e3c9f1b1c7274e8d1219af
DreamHust13 <1506732332@qq.com>
2015-03-26 09:32:23 +0800

    冒泡排序也有细节要注意！

--- BasicAlgorithmic/BubbleSort.cpp
+++ BasicAlgorithmic/BubbleSort.cpp
@@ -1,31 +1,31 @@
 ﻿/*
 冒泡排序
-算法思想：对n个数进行排序，需进行n-1趟排序，每次归为一个数。
+算法思想：对n个数进行排序，需进行n-1趟排序，每次归位一个数。
           每次比较两个相邻的元素，如果他们的顺序错误，就把它们交换过来。
 复杂度：O(N2)
 
 优化：记录是否发生交换（true：是，false：否）
 
 */
+#include "stdafx.h"
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-void BubbleSort(int unsorted[],int n)
+void BubbleSort(int unsorted[],int size)
 {
     bool flag = true;
             
      while(flag)
     {
-        for(int i = 1; i < n; ++i)//n-1次排序
+        for(int i = 0; i < size-1; ++i)//n-1次排序
         {
             flag = false;
-            for(int j=1;j<n-i;j++)
+            for(int j=1; j<size-i; j++)//！！！注意i和j的范围！！
             {
-                if(unsorted[i-1] > unsorted[i])
+                if(unsorted[j-1] > unsorted[j])
                 {
-                    int temp = unsorted[i-1];
-                    unsorted[i-1] = unsorted[i];
-                    unsorted[i] = temp;
+                    swap(unsorted[j-1],unsorted[j]);//是对j，而不是i
                     flag = true;
                  }
             }
