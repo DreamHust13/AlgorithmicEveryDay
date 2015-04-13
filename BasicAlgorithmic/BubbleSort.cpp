97bd726b6cca27202b94f59fec9b40d21d7a0056
DreamHust13 <1506732332@qq.com>
2015-04-13 16:15:21 +0800

    冒泡排序

--- BasicAlgorithmic/BubbleSort.cpp
+++ BasicAlgorithmic/BubbleSort.cpp
@@ -12,43 +12,47 @@
 #include <algorithm>
 using namespace std;
 
-void BubbleSort(int unsorted[],int size)
+void BubbleSort(int unsorted[], int size)
 {
     bool flag = true;
-            
-     while(flag)
+
+    while(flag)
     {
-        for(int i = 0; i < size-1; ++i)//n-1次排序
+        for(int i = 0; i < size - 1; ++i) //n-1次排序
         {
             flag = false;
-            for(int j=1; j<size-i; j++)//！！！注意i和j的范围！！
+
+            for(int j = 1; j < size - i; j++) //！！！注意i和j的范围！！
             {
-                if(unsorted[j-1] > unsorted[j])
+                if(unsorted[j - 1] > unsorted[j])
                 {
-                    swap(unsorted[j-1],unsorted[j]);//是对j，而不是i
+                    swap(unsorted[j - 1], unsorted[j]); //是对j，而不是i
                     flag = true;
-                 }
+                }
             }
+
             if(!flag)
                 return;
-         }
+        }
     }
 }
 
 int  main()
 {
-    int unsorted[] = {3,6,1,4,5};
-    int length = sizeof(unsorted)/sizeof(int);
+    int unsorted[] = {3, 6, 1, 4, 5};
+    int length = sizeof(unsorted) / sizeof(int);
 
-    for(int i=0;i<length;++i)
-       cout<<unsorted[i]<<" ";
-    cout<<endl;
+    for(int i = 0; i < length; ++i)
+        cout << unsorted[i] << " ";
 
-    BubbleSort(unsorted,length);
+    cout << endl;
 
-    for(int i=0;i<length;++i)
-        cout<<unsorted[i]<<" ";
-    cout<<endl;
+    BubbleSort(unsorted, length);
+
+    for(int i = 0; i < length; ++i)
+        cout << unsorted[i] << " ";
+
+    cout << endl;
 
     return 0;
 }
